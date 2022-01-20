41357ab7e23e690f454875b3231822414fbab6b0
Olga Antushevich <antushevich@gmail.com>
2022-01-20 11:57:57 +0200

    feat: add function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+
+  let len = str.length / 2;
+
+  let brackets = bracketsConfig.map((itemArray) => itemArray.join(''));
+
+  for (let i = 0; i < len; i++) {
+
+    for (let b = 0; b < brackets.length; b++) {
+
+      str = str.replace(brackets[b], '');
+    };
+  };
+
+  return !str;
+  // if (str) return false; else return true;
+};
